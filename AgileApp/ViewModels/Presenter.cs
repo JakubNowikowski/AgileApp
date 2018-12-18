9f434e08948b2b06152f65aabbccf4f82054bab5
Dobry wariat <kubanowikowski@gmail.com>
2018-12-18 06:40:32 +0100

    Add RelayCommand

---
+++ AgileApp/ViewModels/Presenter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace AgileApp.ViewModels
+{
+	class Presenter : ObservableObject
+	{
+		private string _userName1;
+
+		public string UserName1
+		{
+			get { return _userName1; }
+			set
+			{
+				_userName1 = value;
+				RaisePropertyChangedEvent(nameof(UserName1));
+			}
+		}
+
+
+
+		public ICommand OpenAddWindow => new DelegateCommand(() =>
+		{
+			
+			UserName1 = "eeeee";
+
+		});
+	}
+}
